aeaecc87ab032a57bedd747f12dd241c53e960dd
aditaurog-stack <adi.taurog@gmail.com>
2025-11-30 10:17:54 +0200

    Render catalog items on public page
    Updated catalog entries for movies, books, music, and TV series.

--- script.js
+++ script.js
@@ -2,25 +2,47 @@
 // Later we will add functions to show them on the page and to edit them.
 
 const catalog = [
-  {
+    {
     id: 1,
     type: "movie",
-    title: "Inception",
-    creator: "Christopher Nolan",
-    year: 2010,
-    imageUrl: "https://via.placeholder.com/150",
-    description: "A mind-bending sci-fi movie about dreams within dreams.",
+    title: "Avengers Infinity War",
+    creator: "Russo Brothers",
+    year: 2018,
+    imageUrl: "https://share.google/images/96XgtUCLP3g1K2bN4",
+    description: "A Group of heroes have to fight to save the world",
+    averageRating: 0,
+    ratingCount: 0
+  }
+  {
+    id: 2,
+    type: "book",
+    title: "Percy Jackson",
+    creator: "Rick Riordan",
+    year: 2005,
+    imageUrl: "https://share.google/images/LubSSJwQCYSDIEtcW",
+    description: "After Percy Finds out he is a demi-god it is up to him to save the world",
     averageRating: 0,
     ratingCount: 0
   },
   {
-    id: 2,
+    id: 3,
     type: "music",
-    title: "Thriller",
-    creator: "Michael Jackson",
-    year: 1982,
-    imageUrl: "https://via.placeholder.com/150",
-    description: "One of the best-selling albums of all time.",
+    title: "Voulez-vous",
+    creator: "abba",
+    year: 1979,
+    imageUrl: "https://share.google/images/YWNSnGuAqA6lYV7hz",
+    description: "One of the best songs of all time.",
+    averageRating: 0,
+    ratingCount: 0
+  },
+  {
+    id: 4,
+    type: "Tv Sries",
+    title: "Stranger Things",
+    creator: "The Duffer Brothers",
+    year: 2016,
+    imageUrl: "https://share.google/images/orXQKjfvUXPWinCk0",
+    description: "After Will goes missing it is up to his friends and family to find him",
     averageRating: 0,
     ratingCount: 0
   },
